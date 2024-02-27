1d1eb05e711aa3b7ee99dbe77afd9d0c46719e42
Sourabh Kumar <20010001030@gateway.edu.in>
2024-02-28 01:52:10 +0530

    implemented Navigation

--- app/src/main/java/com/example/pickupanddrop/presentation/PastScreen.kt
+++ app/src/main/java/com/example/pickupanddrop/presentation/PastScreen.kt
@@ -1,10 +1,12 @@
 package com.example.pickupanddrop.presentation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun PastScreen(
 
 ) {
+    Text(text ="Past Screen")
 
 }
--- app/src/main/java/com/example/pickupanddrop/presentation/UpcomingScreen.kt
+++ app/src/main/java/com/example/pickupanddrop/presentation/UpcomingScreen.kt
@@ -1,10 +1,11 @@
 package com.example.pickupanddrop.presentation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun UpcomingScreen(
     navigateToPast: () -> Unit
 ) {
-
+    Text(text ="Upcoming Screen")
 }
